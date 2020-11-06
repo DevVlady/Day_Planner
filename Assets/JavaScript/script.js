375027f43b280bdd7219e54c6bb5f71f0e1514c8
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-06 17:24:03 -0600

    Played with the save icon in the html file and am now creating the function that will display the color based on the time (current, future, & past).

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,4 +1,13 @@
 //Function that will print the date and time on the webpage
 var date = moment().format('LL');
-$("#date").text(date);
+$("#currentDay").text(date);
 
+//Function that will color the section based on past, present, & future
+var dataColor = document.getElementById("colorcode");
+
+var
+
+var timeSlot = date.getHours();
+$("dataColor").each(function(){
+
+})
