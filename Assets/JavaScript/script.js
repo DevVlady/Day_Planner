0c5eedae292190bfd1e459a1f826594c6444f79c
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-06 19:32:15 -0600

    Got the time function that will color code it based on the present, past, or future time.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -2,12 +2,27 @@
 var date = moment().format('LL');
 $("#currentDay").text(date);
 
+var timeSlotArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+
 //Function that will color the section based on past, present, & future
-var dataColor = document.getElementById("colorcode");
+var timeSlot = document.querySelector(".timeslot");
 
-var
+var currentTimeSlot = moment().format("HH");
 
-var timeSlot = date.getHours();
-$("dataColor").each(function(){
+currentTimeSlot =
 
+
+//Function to display time slot in color (present, future, & past)
+$(".timeslot").each(function(){
+    var currentBlock = $(this).attr("id");
+    console.log(currentTimeSlot, currentBlock);
+    if (currentTimeSlot > currentBlock) {
+        $(this).addClass("past");
+    }
+    else if (currentTimeSlot < currentBlock) {
+        $(this).addClass("future");
+    }
+    else {
+        $(this).addClass("future");
+    }
 })
