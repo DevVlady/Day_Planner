77df8092e7467bd1c72c13d468b8f0ab6d92f813
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-11-09 20:09:36 -0600

    Added the remaining part of the local storage. I have also added a section in the css to make the text in the input field black and a little larger making it easier to read.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,15 +1,10 @@
 $(document).ready(function(){
 
-    dailyTask();
-
     //Function that will print the date and time on the webpage
     var date = moment().format('LLLL');
     $("#currentDay").text(date);
 
     var timeSlotArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-
-    //Function that will color the section based on past, present, & future
-    var timeSlot = document.querySelector(".timeslot");
 
     var currentTimeSlot = moment().format("HH");
 
@@ -28,34 +23,26 @@ $(document).ready(function(){
         }
     });
 
-    var tasks = JSON.parse(localStorage.getItem("taskObject")) || [];
-
-
 
     //Function that will save my content from the input field into the local storage
     $(".saveBtn").on("click", function(){
         //Variable to reflect the hour class set in the html
         var hours = $(this).attr("id");
         //Variable to store the text entered by the user
-        var inputVal = $(this).prev().val().trim();
-        
-        var newTask = {
-            "clock": hours,
-            "input": inputVal
-        }
-        tasks.push(newTask);
-        localStorage.setItem("taskObject", JSON.stringify(tasks));
+        var inputVal = $(this).siblings(".user-event-input").val();
+        //Added the hours and input val as the keys and object to set item in local storage
+        localStorage.setItem(hours, inputVal);
     });
 
 
     //Function to pull data that was stored when the user saved their task in the input field
     function dailyTask () {
-        for (var i = 0; i < currentTimeSlot; i++) {
-            $(".input-slot").val(JSON.parse(localStorage.getItem(timeSlotArray)));
+        for (var i = 0; i < timeSlotArray.length; i++) {
+            $(`#hour${timeSlotArray[i]} .input-slot`).val(localStorage.getItem(timeSlotArray[i]));
         }
     }
-
-
+    //Calling the daily task function
+    dailyTask();
 
 });
 
